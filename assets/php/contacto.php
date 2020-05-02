9dea25f9cefe6193690ceeb5198ebb957f293714
Elian De La Rosa <61727979+VeryTutos@users.noreply.github.com>
2020-05-02 03:58:29 -0400

    Add files via upload

---
+++ assets/php/contacto.php
@@ -0,0 +1,17 @@
+﻿<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+$para = 'gamers05500@gmail.com';
+$titulo = 'Mensaje de un visitante en VeryTutos Server';
+ 
+$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
+ 
+if ($_POST['submit']) {
+if (mail ($para, $titulo, $msjCorreo)) {
+echo 'El mensaje se ha enviado correctamente';
+} else {
+echo 'Por favor llene correctamente los campos';
+}
+}
+<?php
