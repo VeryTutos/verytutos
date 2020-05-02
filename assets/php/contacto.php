ac78f5c0f387c05f57d26e721761a04c2cbcb040
Elian De La Rosa <61727979+VeryTutos@users.noreply.github.com>
2020-05-02 13:48:29 -0400

    Add files via upload

--- assets/php/contacto.php
+++ assets/php/contacto.php
@@ -1,17 +1,21 @@
 ﻿<?php
+
+// Definiendo las variables
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
 $mensaje = $_POST['mensaje'];
-$para = 'gamers05500@gmail.com';
-$titulo = 'Mensaje de un visitante en VeryTutos Server';
+$para = "gamers05500@gmail.com";
+$titulo = "Mensaje de un visitante en VeryTutos Server";
+
  
 $msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
  
+// Si se llenaron los campos correctamente se envia el mensaje, sino emite un error
 if ($_POST['submit']) {
-if (mail ($para, $titulo, $msjCorreo)) {
-echo 'El mensaje se ha enviado correctamente';
-} else {
-echo 'Por favor llene correctamente los campos';
+if (mail ($para, $titulo, $msjCorreo))
+   header('Location:consulta.html'); {
+   echo 'El mensaje se ha enviado correctamente'; }
+else {
+   echo 'Por favor llene correctamente los campos'; }
 }
-}
-<?php
+?>
